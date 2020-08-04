d65285003e8de929b2fff87321ef6a7f0158a900
Thomas Huysmans <thomas@kaizenit.consulting>
2020-08-04 21:06:40 +0200

    Added the ShellServic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { ShellService } from './shell/shell.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private shellService: ShellService) { }
+
   title = 'angular-microfrontend-poc';
+
+  ngOnInit() {
+    this.shellService.init({
+        initialRoute: '/home',
+        outletId: 'content',
+        preload: true,
+        clients: {
+          "project-a": {
+            loaded: false,
+            src: 'assets/projects/client-a/main.js',
+            element: 'client-a',
+            route: '/client-a'
+          }
+        }
+    });
+  }
 }
